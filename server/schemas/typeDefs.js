451bd4162887ce898947097169c8cd0649acef67
cbogart91 <166065354+cbogart91@users.noreply.github.com>
2024-09-11 11:43:00 -0500

    added typeDefs/schemas

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,39 @@
+const typeDefs = `
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    bookCount: String
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: String!
+    authors: [String]
+    description: String
+    title: String!
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    users: [User]
+    user(username: String!): User
+  }
+
+  type Mutation {
+    addUser (username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+
+    userSaveBook(authors: String, description: String, title: String!, bookId: String!, image: String, link: String): User
+    userDeleteBook(bookId: String!): User
+  }
+`;
+
+module.exports = typeDefs;
